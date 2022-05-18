2cb66ccb6e6b97992d7e97040f1942f984a57326
Dan Gill <dan.gill@dgillconsulting.com>
2022-05-18 10:02:57 -0600

    :bug: Search all DCs since lastLogin isn't replicated
    lastLogin is locally stored on each DC and isn't replicated. Script now searchs all DCs and looks for the latest value of this property. Standardize formatting.

--- Set-VendorAccountStatus.ps1
+++ Set-VendorAccountStatus.ps1
@@ -14,17 +14,17 @@
 .LINK
     https://shellgeek.com/get-aduser-filter-examples/
 .LINK
-    https://shellgeek.com/powershell-get-aduser-last-logon/
+    https://serverfault.com/questions/734615/lastlogon-vs-lastlogontimestamp-in-active-directory
 .LINK
     https://shellgeek.com/move-ad-user-to-another-ou/
 .EXAMPLE
     ./Set-VendorAccountStatus.ps1
 .INPUTS
-   None.
+    None.
 .OUTPUTS
-   None.
+    None.
 .EXAMPLE
-   PS> .\Set-VendorAccountStatus.ps1
+    PS> .\Set-VendorAccountStatus.ps1
 #>
 
 # Read settings.json
@@ -37,53 +37,74 @@ $VendorProjectInactiveHours = $Settings.ValidPeriodHours.LongTerm
 $VendorServiceInactiveHours = $Settings.ValidPeriodHours.ShortTerm
 
 function Get-AgedADUsers {
-  [CmdletBinding()]
-  param (
-    # Which OU are we searching?
-    [Parameter(Mandatory, ValueFromPipeline, Position = 0)]
-    [string]
-    $OU,
+    [CmdletBinding()]
+    param (
+        # Which OU are we searching?
+        [Parameter(Mandatory, ValueFromPipeline, Position = 0)]
+        [string]
+        $OU,
 
-    # How long must the account be inactive in hours?
-    [Parameter(Mandatory, ValueFromPipeline, Position = 1)]
-    [int]
-    $InactiveHours,
+        # How long must the account be inactive in hours?
+        [Parameter(Mandatory, ValueFromPipeline, Position = 1)]
+        [int]
+        $InactiveHours,
 
-    # Do we want to process accounts that have never been used?
-    [Parameter(ValueFromPipeline, Position = 2)]
-    [bool]
-    $ProcessUnusedAccounts
-  )
+        # Do we want to process accounts that have never been used?
+        [Parameter(ValueFromPipeline, Position = 2)]
+        [bool]
+        $ProcessUnusedAccounts
+    )
 
-  begin {
-    # Calculate the most recent logon time to permit
-    $FromDate = (Get-Date).AddHours( - ($InactiveHours))
-  }
-  process {
-    if ($ProcessUnusedAccounts) {
-      # Find accounts with a last logon older than $FromDate
-      $users = Get-ADUser -Filter { Enabled -eq $true -and lastLogon -le $FromDate } -SearchBase $OU
-    } else {
-      # Find account with a last logon older than $FromDate that have logged in at least once
-      $users = Get-ADUser -Filter { Enabled -eq $true -and lastLogon -le $FromDate -and lastLogon -ne 0 } -SearchBase $OU
+    begin {
+        # Calculate the most recent logon time to permit
+        $FromDate = (Get-Date).AddHours( - ($InactiveHours))
+
+        # Get list of all domain controllers
+        $DCs = Get-ADDomainController -Filter *
     }
-  }
-  end {
-    return $users
-  }
+    process {
+        if ($ProcessUnusedAccounts) {
+            # Check all DCs because lastLogon is not replicated throughout the domain
+            $users = $DCs | ForEach-Object {
+                # Find accounts with a last logon older than $FromDate
+                Get-ADUser -Filter {
+                    Enabled -eq $true -and lastLogon -le $FromDate
+                } -SearchBase $OU -Properties * -Server $_.name
+                # Group by name and then select the most recent logon from any DC
+            } | Group-Object -Property Name | ForEach-Object {
+                $_.Group | Sort-Object lastLogon -Descending | Select-Object -First 1
+            }
+        } else {
+            # Check all DCs because lastLogon is not replicated throughout the domain
+            $users = $DCs | ForEach-Object {
+                # Find account with a last logon older than $FromDate that have logged in at least once
+                Get-ADUser -Filter {
+                    Enabled -eq $true -and lastLogon -le $FromDate -and lastLogon -ne 0
+                } -SearchBase $OU -Properties * -Server $_.name
+                # Group by name and then select the most recent logon from any DC
+            } | Group-Object -Property Name | ForEach-Object {
+                $_.Group | Sort-Object lastLogon -Descending | Select-Object -First 1
+            }
+        }
+    }
+    end {
+        return $users
+    }
 }
 
 # The script can only run on a DC and the OUs have to exist already.
 if ( Get-WmiObject -Query "select * from Win32_OperatingSystem where ProductType='2'" ) {
-  if ( [adsi]::Exists("LDAP://$VendorServiceOU") -and [adsi]::Exists("LDAP://$VendorProjectOU" ) ) {
-    # Move aged project users to service OU
-    Get-AgedADUsers -OU $VendorProjectOU -InactiveHours $VendorProjectInactiveHours -ProcessUnusedAccounts $false | Move-ADObject -TargetPath $VendorServiceOU
+    if ( [adsi]::Exists("LDAP://$VendorServiceOU") -and [adsi]::Exists("LDAP://$VendorProjectOU" ) ) {
+        # Move aged project users to service OU
+        Get-AgedADUsers -OU $VendorProjectOU -InactiveHours $VendorProjectInactiveHours `
+            -ProcessUnusedAccounts $false | Move-ADObject -TargetPath $VendorServiceOU
 
-    # Disable aged users in service OU
-    Get-AgedADUsers -OU $VendorServiceOU -InactiveHours $VendorServiceInactiveHours -ProcessUnusedAccounts $true | Disable-ADAccount
-  } else {
-    Write-Error -Message 'Unable to locate OU(s). Please verify OU paths.'
-  }
+        # Disable aged users in service OU
+        Get-AgedADUsers -OU $VendorServiceOU -InactiveHours $VendorServiceInactiveHours `
+            -ProcessUnusedAccounts $true | Disable-ADAccount
+    } else {
+        Write-Error -Message 'Unable to locate OU(s). Please verify OU paths.'
+    }
 } else {
-  Write-Error -Message 'This script can only run on a domain controller.'
+    Write-Error -Message 'This script can only run on a domain controller.'
 }
